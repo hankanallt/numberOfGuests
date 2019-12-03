88cb02c747c691e5a8c0772b51c5e9cf42f79b2c
Harkanwal Singh <hankanallt@gmail.com>
2019-12-03 12:32:21 +0100

    First Commit

---
+++ src/numberofguests/NumberOfGuests.java
@@ -0,0 +1,34 @@
+
+package numberofguests;
+
+import java.util.Scanner;
+
+
+public class NumberOfGuests {
+
+    
+    public static void main(String[] args) {
+        
+       
+        for (int numberOfGuests = 0; numberOfGuests < 30; numberOfGuests++) {
+            
+        }
+        Scanner sc = new Scanner(System.in);
+        
+        int numberOfGuests = 0;
+        if (numberOfGuests < 10) {
+            System.out.println("We have a family gathering");
+        }
+        else if (numberOfGuests > 20){
+        System.out.println("We have a Party");
+    }
+        else if ( numberOfGuests > 30){
+        System.out.println("We Rock");
+
+    }
+    }
+
+}
+    
+    
+
